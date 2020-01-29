a59eb3d727decc1dcf57dcdb38d337c9a38be692
Daniel Reigada <daniel@codacy.com>
2020-01-29 15:43:52 +0000

    clean: Improve Main class

--- src/main/scala/com/codacy/clangtidy/Main.scala
+++ src/main/scala/com/codacy/clangtidy/Main.scala
@@ -1,15 +1,15 @@
 package com.codacy.clangtidy
 
-import io.circe.generic.AutoDerivation
+object Main {
 
-object Main extends App with AutoDerivation {
+  def main(args: Array[String]): Unit = {
+    // TODO make this dynamic
+    val toolName = "clang-tidy"
 
-  // TODO make this dynamic
-  val toolName = "clang-tidy"
-
-  // TODO allow to read report from file
-  val lines = scala.io.Source.stdin.getLines().to(LazyList)
-  val jsonString = new Converter(toolName).convert(lines)
-  println(jsonString)
+    // TODO allow to read report from file
+    val lines = scala.io.Source.stdin.getLines().to(LazyList)
+    val jsonString = new Converter(toolName).convert(lines)
+    println(jsonString)
+  }
 
 }
